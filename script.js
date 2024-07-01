660871a63b51538e4e5ae6de06dc0c638858cbce
KARTHI3106 <ganeshmurthy2020@outlook.com>
2024-07-01 13:59:35 +0530

    minor fixes

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
-const taskInput = document.getElementById('task-input');
-const addTaskBtn = document.getElementById('add-task-btn');
-const taskList = document.getElementById('task-list');
-const modal = document.querySelector('.modal-enter-task');
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
+const modal = document.querySelector(".modal-enter-task");
 
+const createTaskElement = (taskName, index) => {
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Remove";
 
-const createTaskElement = taskName => {
-  const removeBtn = document.createElement('button');
-  removeBtn.textContent = 'Remove';
-
-  const task = document.createElement('li');
-  task.textContent = taskName;
+  const task = document.createElement("li");
+  task.textContent = `${index}: ${taskName}`;
   task.appendChild(removeBtn);
 
-  taskInput.value = '';
+  taskInput.value = "";
 
   // Remove task.
-  removeBtn.addEventListener('click', () => {
+  removeBtn.addEventListener("click", () => {
     taskList.removeChild(task);
   });
 
   return task;
 };
 
-addTaskBtn.addEventListener('click', () => {
-  if (taskInput.value !== '') {
-    const task = createTaskElement(taskInput.value);
+addTaskBtn.addEventListener("click", () => {
+  if (taskInput.value !== "") {
+    const task = createTaskElement(taskInput.value, () => 1);
     taskList.appendChild(task);
   } else {
-    modal.classList.remove('hidden');
+    modal.classList.remove("hidden");
     setTimeout(() => {
-      modal.classList.add('hidden');
+      modal.classList.add("hidden");
     }, 2000);
   }
 });
 
 // To check whether the taskinput has placeholder text. If yes, it removes the text else not
 if (taskInput) {
-  const updatePlaceHolder = text => {
+  const updatePlaceHolder = (text) => {
     taskInput.placeholder = text;
   };
 
-  taskInput.addEventListener('focus', () => {
-    updatePlaceHolder('');
+  taskInput.addEventListener("focus", () => {
+    updatePlaceHolder("");
   });
 
-  taskInput.addEventListener('blur', () => {
-    updatePlaceHolder('New Task');
+  taskInput.addEventListener("blur", () => {
+    updatePlaceHolder("New Task");
   });
 }
